4ff3becc1dbe20007081381932a2dd514529ea24
Lars Corneliussen <lars@corneliussen.de>
2011-07-05 21:49:30 +0200

    Added Linq2Dictionary-Minimod

---
+++ Linq2Dictionary/Linq2DictionaryMinimod.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Minimod.Linq2Dictionary
+{
+    /// <summary>
+    /// <h1>Minimod.Linq2Dictionary, Version 0.9.0, Copyright © Lars Corneliussen 2011</h1>
+    /// <para>Extends the Dictionary with some linquish Methods like Union, (more to come).</para>
+    /// </summary>
+    /// <remarks>
+    /// Licensed under the Apache License, Version 2.0; you may not use this file except in compliance with the License.
+    /// http://www.apache.org/licenses/LICENSE-2.0
+    /// </remarks>
+    public static class Linq2DictionaryMinimod
+    {
+        /// <summary>
+        /// Unions the key-value-pairs of two dictionaries into one.
+        /// </summary>
+        public static IDictionary<TKey, TCol> Union<TKey, TCol>(this IDictionary<TKey, TCol> first, IDictionary<TKey, TCol> second)
+        {
+            if (first == null) throw new ArgumentNullException("first");
+            if (second == null) throw new ArgumentNullException("second");
+
+            if (first.Count == 0 && second.Count == 0)
+                return new Dictionary<TKey, TCol>(0);
+
+            if (first.Count == 0)
+                return second.ToDictionary(); // actually a clone
+
+            if (second.Count == 0)
+                return first.ToDictionary(); // actually a clone
+
+            return first.ToArray().Union(second).ToDictionary();
+        }
+
+        /// <summary>
+        /// Creates a dictionary from enumerable key-value-pairs.
+        /// </summary>
+        public static IDictionary<TKey, TCol> ToDictionary<TKey, TCol>(this IEnumerable<KeyValuePair<TKey, TCol>> pairs)
+        {
+            return pairs.ToDictionary(kv => kv.Key, kv => kv.Value);
+        }
+    }
+}
---
+++ Linq2Dictionary/Linq2DictionaryTests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace Minimod.Linq2Dictionary
+{
+    [TestFixture]
+    public class Linq2DictionaryTests
+    {
+        [Test]
+        public void Union_FirstIsNull_ThrowsArgumentException()
+        {
+            IDictionary<string, string> first = null;
+
+
+            first.Executing(f => f.Union(new Dictionary<string, string>()))
+                .Throws<ArgumentNullException>();
+        }
+
+        [Test]
+        public void Union_SecondIsNull_ThrowsArgumentException()
+        {
+            IDictionary<string, string> first = new Dictionary<string, string>();
+
+
+            first.Executing(f => f.Union(null))
+                .Throws<ArgumentNullException>();
+        }
+
+        [Test]
+        public void Union_BothEmpty_IsEmpty()
+        {
+            IDictionary<string, string> first = new Dictionary<string, string>();
+            IDictionary<string, string> second = new Dictionary<string, string>();
+
+            IDictionary<string, string> result = first.Union(second);
+            result.Should().Be.Empty();
+        }
+
+        [Test]
+        public void Union_SecondEmpty_ContentEqualsFirst()
+        {
+            IDictionary<string, string> first = new Dictionary<string, string>()
+                                                    {
+                                                        {"a", "1"}
+                                                    };
+
+            IDictionary<string, string> second = new Dictionary<string, string>();
+
+            IDictionary<string, string> result = first.Union(second);
+            result.Should().Have.SameValuesAs(first);
+        }
+
+        [Test]
+        public void Union_FirstEmpty_ContentEqualsFirst()
+        {
+            IDictionary<string, string> first = new Dictionary<string, string>();
+
+            IDictionary<string, string> second = new Dictionary<string, string>(){
+                                                        {"a", "1"}
+                                                    };
+
+            IDictionary<string, string> result = first.Union(second);
+            result.Should().Have.SameValuesAs(second);
+        }
+
+        [Test]
+        public void Union_DistinctKeys_ContainsBoth()
+        {
+            IDictionary<string, string> first = new Dictionary<string, string>(){
+                                                        {"a", "1"}
+                                                    };
+
+            IDictionary<string, string> second = new Dictionary<string, string>{
+                                                        {"b", "2"}
+                                                    };
+
+            IDictionary<string, string> result = first.Union(second);
+            result.Should().Have.SameValuesAs(new Dictionary<string, string>{
+                                                        {"a", "1"},
+                                                        {"b", "2"}
+                                                    });
+        }
+
+        [Test]
+        public void Union_KeyOverlap_Throws()
+        {
+            IDictionary<string, string> first = new Dictionary<string, string>(){
+                                                        {"a", "1"}
+                                                    };
+
+            IDictionary<string, string> second = new Dictionary<string, string>{
+                                                        {"a", "2"}
+                                                    };
+
+            first.Executing(_ => _.Union(second))
+                .Throws<ArgumentException>();
+        }
+    }
+}
