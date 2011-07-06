47792f57749d18021150ddb03b3d6b02d0689f46
Mike Bild <mike@mikebild.com>
2011-07-06 21:48:45 +0200

    some rx message bus refactorings

--- MiniMods/Flattener/FlattenerMinimod.cs
+++ MiniMods/Flattener/FlattenerMinimod.cs
@@ -1,8 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Minimod.LinqExtentions
+namespace Minimod.Flattener
 {
+    /// <summary>
+    /// Minimod.Flattener, Version 0.0.1
+    /// <para>A small hierarchy flattener for IEnumerable´1.</para>
+    /// </summary>
+    /// <remarks>
+    /// Licensed under the Apache License, Version 2.0; you may not use this file except in compliance with the License.
+    /// http://www.apache.org/licenses/LICENSE-2.0
+    /// </remarks>
     public static class FlattenerMinimod
     {
         public static IEnumerable<T> Flatten<T>(this IEnumerable<T> source, Func<T, IEnumerable<T>> flattenBy)
--- MiniMods/RxMessageBus/RxMessageBusMinimod.cs
+++ MiniMods/RxMessageBus/RxMessageBusMinimod.cs
@@ -5,13 +5,21 @@ using System.Reactive.Subjects;
 
 namespace Minimod.RxMessageBus
 {
-    public class RxMessageBus
+    /// <summary>
+    /// Minimod.RxMessageBus, Version 0.0.1
+    /// <para>A minimod event-/message bus.</para>
+    /// </summary>
+    /// <remarks>
+    /// Licensed under the Apache License, Version 2.0; you may not use this file except in compliance with the License.
+    /// http://www.apache.org/licenses/LICENSE-2.0
+    /// </remarks>
+    public class RxMessageBusMinimod
     {
-        private static RxMessageBus _defaultInstance;
+        private static RxMessageBusMinimod _defaultInstance;
         private readonly Subject<object> _subject = new Subject<object>();
 
-        public static RxMessageBus Default { get { return _defaultInstance ?? (_defaultInstance = new RxMessageBus()); } }
-        public static void OverrideDefault(RxMessageBus newMessenger) { _defaultInstance = newMessenger; }
+        public static RxMessageBusMinimod Default { get { return _defaultInstance ?? (_defaultInstance = new RxMessageBusMinimod()); } }
+        public static void OverrideDefault(RxMessageBusMinimod newMessenger) { _defaultInstance = newMessenger; }
         public static void Reset() { _defaultInstance = null; }
 
         public IDisposable Register<T>(Action<T> action)
