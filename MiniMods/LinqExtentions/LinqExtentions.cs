914381ed334875b8df79b0b35b3ec5bf43f3e44f
Mike Bild <mike@mikebild.com>
2011-07-06 15:29:38 +0200

    Flatten - a small flatten linq extention

---
+++ MiniMods/LinqExtentions/LinqExtentions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Minimod.LinqExtentions
+{
+    public static class LinqExtensions
+    {
+        public static IEnumerable<T> Flatten<T>(this IEnumerable<T> source, Func<T, IEnumerable<T>> flattenBy)
+        {
+            if (source == null)
+                yield break;
+
+            foreach (var value in source)
+            {
+                yield return value;
+                foreach (var child in flattenBy(value).Flatten<T>(flattenBy))
+                    yield return child;
+            }
+        }
+    }
+}
