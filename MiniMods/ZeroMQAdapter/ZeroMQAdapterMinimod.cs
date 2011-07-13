c4545900e805df9f11474bde13e76a14b2edf8b6
Mike Bild <mike@mikebild.com>
2011-07-13 13:08:56 +0200

    ZeroMQ messaging adapter added, nuspec descriptiton changes for RxMessageBus

---
+++ MiniMods/ZeroMQAdapter/ZeroMQAdapterMinimod.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading;
+using Newtonsoft.Json;
+using ZMQ;
+
+namespace Minimod.ZeroMQAdapter
+{
+    /// <summary>
+    /// Minimod.ZeroMQAdapter, Version 0.0.1
+    /// <para>A minimod for handling messaging with ZeroMQ, Json and Rx Framework.</para>
+    /// </summary>
+    /// <remarks>
+    /// Licensed under the Apache License, Version 2.0; you may not use this file except in compliance with the License.
+    /// http://www.apache.org/licenses/LICENSE-2.0
+    /// </remarks>
+    public class ZeroMQAdapterMinimod : IDisposable
+    {
+        private readonly Context _subContext;
+        private readonly Context _pubContext;
+        private readonly Socket _subSocket;
+        private readonly Socket _pubSocket;
+        private readonly Subject<object> _subject;
+        private readonly IDisposable _subscription;
+        private readonly Guid _correlationId;
+
+        public ZeroMQAdapterMinimod(string subsciptionAddress, string publishAddress)
+        {
+            _correlationId = Guid.NewGuid();
+            _subContext = new Context(1);
+            _pubContext = new Context(1);
+            _subject = new Subject<object>();
+            _subSocket = _subContext.Socket(SocketType.SUB);
+            _pubSocket = _pubContext.Socket(SocketType.PUB);
+
+            _subSocket.Connect(subsciptionAddress);
+            _pubSocket.Connect(publishAddress);
+
+            _subSocket.Subscribe(String.Empty, Encoding.UTF8);
+
+            _subscription = Observable
+                .Interval(TimeSpan.FromMilliseconds(500))
+                .Subscribe(_ => _subject.OnNext(_subSocket.Recv(Encoding.UTF8)));
+        }
+
+        public IDisposable Register<T>(Action<T> action)
+        {
+            return Register(action, new SynchronizationContextScheduler(SynchronizationContext.Current));
+        }
+
+        public IDisposable Register<T>(Action<T> action, IScheduler scheduler)
+        {
+            return _subject
+                .Where(message => !message.ToString().Contains(_correlationId.ToString()))
+                .Select<object, dynamic>(jsonMessage => JsonConvert.DeserializeObject<ExpandoObject>(jsonMessage.ToString(), new Newtonsoft.Json.Converters.ExpandoObjectConverter()))                
+                .Select(message =>
+                            {
+                                try
+                                {
+                                    ((IDictionary<string, object>)message).Remove("CorrelationId");
+                                    ((IDictionary<string, object>)message).Remove("CorrelationTimeStamp");
+                                    var serializeObject = JsonConvert.SerializeObject(message);
+                                    return JsonConvert.DeserializeObject<T>(serializeObject, new JsonSerializerSettings() { MissingMemberHandling = MissingMemberHandling.Error });
+                                }
+                                catch { return message; }
+                            })
+                .OfType<T>()
+                .ObserveOn(scheduler)
+                .Subscribe(action);
+        }
+
+        public void Send(dynamic value)
+        {
+            var serializeObject = JsonConvert.SerializeObject(value);
+            var deserializeObject = JsonConvert.DeserializeObject<ExpandoObject>(serializeObject, new Newtonsoft.Json.Converters.ExpandoObjectConverter());
+            deserializeObject.CorrelationTimeStamp = DateTime.Now;
+            deserializeObject.CorrelationId = _correlationId.ToString();
+            var message = JsonConvert.SerializeObject(deserializeObject);
+            _pubSocket.Send(message, Encoding.UTF8);
+        }
+
+        public void Dispose()
+        {
+            _subject.OnCompleted();
+            _subscription.Dispose();
+            _pubSocket.Dispose();
+            _subSocket.Dispose();
+            _subContext.Dispose();
+        }
+    }
+}
