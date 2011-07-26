ce279a1b611bb16ce5c65aeed4392b5c7f133a08
Lars Corneliussen <me@lcorneliussen.de>
2011-07-26 18:49:23 +0200

    Added JoinStringsMinimod, which fluently joines Enumerables using String.Join under the hood.

---
+++ MiniMods/JoinStrings/JoinStringsMinimod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Minimod.JoinStrings
+{
+    /// <summary>
+    /// <h1>Minimod.JoinStrings, Version 0.9.1, Copyright © Lars Corneliussen 2011</h1>
+    /// <para>A minimod for fluently joining Enumerables using String.Join(..).</para>
+    /// </summary>
+    /// <remarks>
+    /// Licensed under the Apache License, Version 2.0; you may not use this file except in compliance with the License.
+    /// http://www.apache.org/licenses/LICENSE-2.0
+    /// </remarks>
+    public static class JoinStringsMinimod
+    {
+        public static string JoinStringsWith(this IEnumerable values, string separator)
+        {
+            return string.Join(separator, values.Cast<object>().Select(_ => _.ToString()).ToArray());
+        }
+
+        public static string JoinStringsWith(this IEnumerable<string> values, string separator)
+        {
+            return string.Join(separator, values.ToArray());
+        }
+    }
+}
---
+++ MiniMods/JoinStrings/JoinStringsMinimodTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace Minimod.JoinStrings
+{
+    [TestFixture]
+    public class JoinStringsMinimodTests
+    {
+        // TODO
+    }
+}
