74499a72183336a3df6e3a279aa5d34adca6bf3a
Lars Corneliussen <me@lcorneliussen.de>
2011-08-01 20:05:30 +0200

    Added FluentGenerics Minimod

---
+++ MiniMods/FluentGenerics/FluentGenericsMinimod.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Minimod.PrettyTypeSignatures;
+
+namespace Minimod.FluentGenerics
+{
+    /// <summary>
+    /// <h1>Minimod.FluentGenerics, Version 0.9.0, Copyright © Lars Corneliussen 2011</h1>
+    /// <para>A minimod for fluently interacting with genric types.</para>
+    /// </summary>
+    /// <remarks>
+    /// Licensed under the Apache License, Version 2.0; you may not use this file except in compliance with the License.
+    /// http://www.apache.org/licenses/LICENSE-2.0
+    /// </remarks>
+    public static class FluentGenericsMinimod
+    {
+        public static bool IsOfGenericType(this Type type, Type genericTypeDefinition)
+        {
+            return findGenericTypesForDefinition(type, genericTypeDefinition) != null;
+        }
+
+        public static Type[] GetGenericTypesFor(this Type type, Type genericTypeDefinition)
+        {
+            Type[] foundTypes = findGenericTypesForDefinition(type, genericTypeDefinition);
+
+            if (foundTypes.Length == 0)
+            {
+                throw new ArgumentException(
+                    string.Format(
+                                     "The type '{0}' is not a generic type of {1}.",
+                                     type.GetPrettyName(),
+                                     genericTypeDefinition.GetPrettyName()),
+                    "type");
+            }
+
+            return foundTypes;
+        }
+
+        public static Type GetGenericTypeFor(this Type type, Type genericTypeDefinition)
+        {
+            Type[] foundTypes = GetGenericTypesFor(type, genericTypeDefinition);
+
+            if (foundTypes.Length > 1)
+            {
+                throw new ArgumentException(
+                    string.Format(
+                                     "The type '{0}' has multiple implementations of {1}:{2}",
+                                     type.GetPrettyName(),
+                                     genericTypeDefinition.GetPrettyName(),
+                                     string.Join(", ", foundTypes.Select(t => t.GetPrettyName()).ToArray())
+                                     ),
+                    "type");
+            }
+
+            return foundTypes[0];
+        }
+
+        private static Type[] findGenericTypesForDefinition(Type type, Type genericTypeDefinition)
+        {
+            if (type == null) throw new ArgumentNullException("type");
+            if (genericTypeDefinition == null) throw new ArgumentNullException("genericTypeDefinition");
+
+            if (!genericTypeDefinition.IsGenericTypeDefinition)
+            {
+                throw new ArgumentException(
+                    string.Format(
+                                     "The type '{0}' is not a generic type definition.", genericTypeDefinition.FullName),
+                    "genericTypeDefinition");
+            }
+
+            Type matchingBaseType = getTypeHierarchy(type)
+                .Reverse()
+                .Where(t => t.IsGenericType && genericTypeDefinition == t.GetGenericTypeDefinition())
+                .FirstOrDefault();
+
+            if (matchingBaseType != null)
+            {
+                return new[] { matchingBaseType };
+            }
+
+            TypeFilter filter = (i, o) => i.IsGenericType && genericTypeDefinition == i.GetGenericTypeDefinition();
+            return type
+                .FindInterfaces(filter, null)
+                .ToArray();
+        }
+
+        public static Type[] GetGenericArgumentsFor(this Type type, Type genericTypeDefinition)
+        {
+            return GetGenericTypeFor(type, genericTypeDefinition).GetGenericArguments();
+        }
+
+        /// <summary>
+        /// Beschafft die Typ-Hierarchie von object bis <paramref name="type"/>.
+        /// </summary>
+        /// <param name="type">
+        /// Der konkrete Typ, für den die Hierarchie zu beschaffen ist.
+        /// </param>
+        /// <returns>
+        /// Eine Liste von Typen von object bis <paramref name="type"/>.
+        /// </returns>
+        private static IEnumerable<Type> getTypeHierarchy(this Type type)
+        {
+            var types = new List<Type>();
+            for (Type current = type; current != null; current = current.BaseType)
+            {
+                types.Add(current);
+            }
+
+            // drehe sie dann um
+            types.Reverse();
+
+            return types;
+        }
+    }
+}
---
+++ MiniMods/FluentGenerics/FluentGenericsMinimodTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Minimod.JoinStrings;
+using Minimod.PrettyTypeSignatures;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace Minimod.FluentGenerics
+{
+    [TestFixture]
+    public class FluentGenericsMinimodTests
+    {
+        [Test]
+        public void PrintExamples()
+        {
+            printExample(typeof(List<int>), typeof(IEnumerable<>));
+            printExample(typeof(Dictionary<int, string>), typeof(IDictionary<,>));
+        }
+
+        private static void printExample(Type type, Type genTypeDef)
+        {
+            bool isOfGenericType = type.IsOfGenericType(genTypeDef);
+
+            Console.WriteLine("Is {0} an {1}? {2}",
+                              type.GetPrettyName(),
+                              genTypeDef.GetPrettyName(),
+                              isOfGenericType ? "yes." : "no.");
+
+            if (isOfGenericType)
+            {
+                Console.WriteLine("If it is, what are the type parameters? "
+                                  + type.GetGenericArgumentsFor(genTypeDef)
+                                        .Select(_ => _.GetPrettyName())
+                                        .JoinStringsWith(", "));
+            }
+        }
+
+        [Test]
+        public void IsOfGenericType_ListOfInts_IsEnumerableOfInts()
+        {
+            typeof(List<int>).IsOfGenericType(typeof(IEnumerable<>))
+                .Should().Be.True();
+        }
+
+        [Test]
+        public void IsOfGenericType_ListOfInts_IsIListOfInts()
+        {
+            typeof(List<int>).IsOfGenericType(typeof(IList<>))
+                .Should().Be.True();
+        }
+
+        [Test]
+        public void GetGenericArgumentsFor_ListOfInts_ArgForIListShouldBeAnInt()
+        {
+            typeof(List<int>).GetGenericArgumentsFor(typeof(IList<>))
+                .Should().Have.SameSequenceAs(typeof(int));
+        }
+
+        [Test]
+        public void GetGenericTypeFor_ListOfInts_GenArgumentForIListIsInt()
+        {
+            typeof(List<int>).GetGenericTypeFor(typeof(IList<>))
+                .IsGenericType.Should().Be.True();
+        }
+    }
+}
