e3a6a121ada613888b61477c1e6d46be4ce8fba7
Mike Bild <mike.bild@googlemail.com>
2011-11-15 16:06:50 +0100

    fix typo issue

--- MiniMods/PatternMatching/PatternMatchingMinimod.cs
+++ MiniMods/PatternMatching/PatternMatchingMinimod.cs
@@ -5,7 +5,7 @@ namespace Minimod.PatternMatching
 {
     /// <summary>
     /// Minimod.PatternMatching, Version 0.0.1
-    /// <para>A minimod for easy to use pattern pattern matching in CSharp.</para>
+    /// <para>A minimod for easy to use pattern matching in CSharp.</para>
     /// </summary>
     /// <remarks>
     /// Licensed under the Apache License, Version 2.0; you may not use this file except in compliance with the License.
