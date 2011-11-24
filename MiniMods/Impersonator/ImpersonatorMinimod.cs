b0450b4d44ceb293eceadc406bcf25326c862247
Mike Bild <mike.bild@googlemail.com>
2011-11-24 15:06:37 +0100

    Impersonator minimod init

---
+++ MiniMods/Impersonator/ImpersonatorMinimod.cs
@@ -0,0 +1,130 @@
+using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+
+namespace Minimod.Impersonator
+{
+    /// <summary>
+    /// Minimod.Impersonator, Version 0.0.1
+    /// <para>A minimod for impersonation with network share support.</para>
+    /// </summary>
+    /// <remarks>
+    /// Licensed under the Apache License, Version 2.0; you may not use this file except in compliance with the License.
+    /// http://www.apache.org/licenses/LICENSE-2.0
+    /// </remarks>
+    public class Impersonator :
+        IDisposable
+    {
+        public Impersonator(
+            string userName,
+            string domainName,
+            string password)
+        {
+            ImpersonateValidUser(userName, domainName, password);
+        }
+
+        public void Dispose()
+        {
+            UndoImpersonation();
+        }
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern int LogonUser(
+            string lpszUserName,
+            string lpszDomain,
+            string lpszPassword,
+            int dwLogonType,
+            int dwLogonProvider,
+            ref IntPtr phToken);
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int DuplicateToken(
+            IntPtr hToken,
+            int impersonationLevel,
+            ref IntPtr hNewToken);
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool RevertToSelf();
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        private static extern bool CloseHandle(
+            IntPtr handle);
+
+        private WindowsImpersonationContext _impersonationContext;
+
+        public const int LOGON32_LOGON_INTERACTIVE = 2;
+        public const int LOGON32_LOGON_NETWORK = 3;
+        public const int LOGON32_LOGON_BATCH = 4;
+        public const int LOGON32_LOGON_SERVICE = 5;
+        public const int LOGON32_LOGON_UNLOCK = 7;
+        public const int LOGON32_LOGON_NETWORK_CLEARTEXT = 8;
+        public const int LOGON32_LOGON_NEW_CREDENTIALS = 9;
+
+        public const int LOGON32_PROVIDER_DEFAULT = 0;
+        public const int LOGON32_PROVIDER_WINNT35 = 1;
+        public const int LOGON32_PROVIDER_WINNT40 = 2;
+        public const int LOGON32_PROVIDER_WINNT50 = 3; 
+
+        private void ImpersonateValidUser(
+            string userName,
+            string domain,
+            string password)
+        {
+            IntPtr token = IntPtr.Zero;
+            IntPtr tokenDuplicate = IntPtr.Zero;
+
+            try
+            {
+                if (RevertToSelf())
+                {
+                    if (LogonUser(
+                        userName,
+                        domain,
+                        password,
+                        LOGON32_LOGON_NEW_CREDENTIALS,
+                        LOGON32_PROVIDER_DEFAULT,
+                        ref token) != 0)
+                    {
+                        if (DuplicateToken(token, 2, ref tokenDuplicate) != 0)
+                        {
+                            var tempWindowsIdentity = new WindowsIdentity(tokenDuplicate);
+                            _impersonationContext = tempWindowsIdentity.Impersonate();
+                        }
+                        else
+                        {
+                            throw new Win32Exception(Marshal.GetLastWin32Error());
+                        }
+                    }
+                    else
+                    {
+                        throw new Win32Exception(Marshal.GetLastWin32Error());
+                    }
+                }
+                else
+                {
+                    throw new Win32Exception(Marshal.GetLastWin32Error());
+                }
+            }
+            finally
+            {
+                if (token != IntPtr.Zero)
+                {
+                    CloseHandle(token);
+                }
+                if (tokenDuplicate != IntPtr.Zero)
+                {
+                    CloseHandle(tokenDuplicate);
+                }
+            }
+        }
+
+        private void UndoImpersonation()
+        {
+            if (_impersonationContext != null)
+            {
+                _impersonationContext.Undo();
+            }
+        }
+    }
+}
