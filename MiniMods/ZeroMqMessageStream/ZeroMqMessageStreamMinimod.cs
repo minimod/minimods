b8101b0225d928246adbe0cf61028e1c9d9fabc8
Mike Bild <mike.bild@googlemail.com>
2012-02-16 16:47:57 +0100

    Update MiniMods/ZeroMqMessageStream/ZeroMqMessageStreamMinimod.cs

--- MiniMods/ZeroMqMessageStream/ZeroMqMessageStreamMinimod.cs
+++ MiniMods/ZeroMqMessageStream/ZeroMqMessageStreamMinimod.cs
@@ -21,10 +21,9 @@ namespace Minimod.ZeroMqMessageStream
     /// Licensed under the Apache License, Version 2.0; you may not use this file except in compliance with the License.
     /// http://www.apache.org/licenses/LICENSE-2.0
     /// </remarks>
-    public interface IMessageStreamContext
+    public interface IMessageStreamContext : IMessageStream
     {
-        void Send<T>(T value);
-        void Send<T>(T message, params string[] publishAddresses);
+        void Send<T>(T values, params string[] publishAddresses);
     }
 
     public class MessageNotSendException : Exception
@@ -43,55 +42,14 @@ namespace Minimod.ZeroMqMessageStream
         public IMessageStreamContext MessageStreamContext { get; set; }
     }
 
-    public class ZeroMqMessageStream : IMessageStreamContext, IDisposable
+    public class ZeroMqMessageStream : IObservable<object>, IMessageStreamContext, IDisposable
     {
-        private readonly Subject<object> _stream;
+        private readonly Subject<object> _messageStream = new Subject<object>();
         private readonly Guid _correlationId;
         private readonly ZmqContext _context;
         private readonly ZmqSocket _subSocket;
         private readonly ZmqSocket _pubSocket;
         private readonly IScheduler _scheduler;
-
-        public IObservable<object> MessageStream
-        {
-            get
-            {
-                return _stream
-                    .SubscribeOn(_scheduler)
-                    .Select<object, dynamic>(jsonMessage => JsonConvert.DeserializeObject<ExpandoObject>(jsonMessage.ToString(), new Newtonsoft.Json.Converters.ExpandoObjectConverter()))
-                    .Select<object, object>(message =>
-                                                {
-                                                    try
-                                                    {
-                                                        //resolve message type for deserializer
-                                                        var messageTypeDescription = ((IDictionary<string, object>)message)
-                                                                                            .Single(x => x.Key == "MessageTypeFullName")
-                                                                                            .Value
-                                                                                            .ToString();
-                                                        var messageType = Type.GetType(messageTypeDescription);
-                                                        ((IDictionary<string, object>)message).Remove("MessageTypeFullName");
-
-                                                        //i don't need it yet
-                                                        ((IDictionary<string, object>)message).Remove("CorrelationId");
-                                                        ((IDictionary<string, object>)message).Remove("CorrelationTimeStamp");
-
-                                                        //deserialize
-                                                        var cleanedMessage = JsonConvert.SerializeObject(message);
-                                                        var deserializeObject = JsonConvert.DeserializeObject(cleanedMessage, messageType, new JsonSerializerSettings { MissingMemberHandling = MissingMemberHandling.Error, NullValueHandling = NullValueHandling.Include });
-
-                                                        //add message stream context if possible
-                                                        if (deserializeObject.GetType().BaseType == typeof(DocumentMessage))
-                                                            ((DocumentMessage)deserializeObject).MessageStreamContext = this;
-
-                                                        return deserializeObject;
-                                                    }
-                                                    catch
-                                                    {
-                                                        return message;
-                                                    }
-                                                });
-            }
-        }
 
         public ZeroMqMessageStream(params string[] publishAddresses)
             : this(new EventLoopScheduler(), String.Empty, publishAddresses)
@@ -112,7 +70,6 @@ namespace Minimod.ZeroMqMessageStream
         public ZeroMqMessageStream(IScheduler scheduler, string subsciptionAddress, params string[] publishAddresses)
         {
             _scheduler = scheduler;
-            _stream = new Subject<object>();
             _correlationId = Guid.NewGuid();
             _context = ZmqContext.Create();
             _subSocket = _context.CreateSocket(SocketType.SUB);
@@ -136,33 +93,30 @@ namespace Minimod.ZeroMqMessageStream
                                                      if (zmqMessage.FrameCount >= 0 && zmqMessage.TotalSize > 1 && zmqMessage.IsComplete)
                                                      {
                                                          var message = Encoding.UTF8.GetString(zmqMessage.First());
-                                                         _stream.OnNext(message);
+                                                         _messageStream.OnNext(message);
                                                          Debug.WriteLine("Correlation ID: {0} - message received with status {1}", _correlationId, _subSocket.ReceiveStatus);
                                                      }
                                                  }
                                              });
         }
 
-        public void Send<T>(T message)
+        public void Send<T>(T value) where T : IMessage
         {
-            SendInternal(message, _pubSocket);
+            SendInternal(value, _pubSocket);
         }
-
-        public void Send<T>(T message, params string[] publishAddresses)
+        public void Send<T>(T value, params string[] publishAddresses)
         {
             foreach (var publishAddress in publishAddresses)
                 _pubSocket.Connect(publishAddress);
-            SendInternal(message, _pubSocket);
+            SendInternal(value, _pubSocket);
         }
-
-        private void SendInternal<T>(T message, ZmqSocket socket)
+        private void SendInternal<T>(T value, ZmqSocket socket)
         {
-            var status = socket.SendFrame(new Frame(SerializeMessage(message)));
+            var status = socket.SendFrame(new Frame(SerializeMessage(value)));
             if (status.ToString() != "Sent")
                 throw new MessageNotSendException(status.ToString());
             Debug.WriteLine("Correlation ID: {0} - message was {1}", _correlationId, status);
         }
-
         private byte[] SerializeMessage<T>(T message)
         {
             var value = message as dynamic;
@@ -180,13 +134,53 @@ namespace Minimod.ZeroMqMessageStream
             var serializedMessage = JsonConvert.SerializeObject(deserializeObject);
             return Encoding.UTF8.GetBytes(serializedMessage);
         }
-
         public void Dispose()
         {
-            _stream.OnCompleted();
+            _messageStream.OnCompleted();
             _subSocket.Close();
             _pubSocket.Close();
             _context.Terminate();
+            _messageStream.Dispose();
+        }
+
+        public IDisposable Subscribe(IObserver<object> observer)
+        {
+            return _messageStream
+                .SubscribeOn(_scheduler)
+                .Select<object, dynamic>(jsonMessage => JsonConvert.DeserializeObject<ExpandoObject>(jsonMessage.ToString(), new Newtonsoft.Json.Converters.ExpandoObjectConverter()))
+                .Select<object, object>(message =>
+                                            {
+                                                try
+                                                {
+                                                    //resolve message type for deserializer
+                                                    var messageTypeDescription = ((IDictionary<string, object>)message)
+                                                        .Single(x => x.Key == "MessageTypeFullName")
+                                                        .Value
+                                                        .ToString();
+                                                    var messageType = Type.GetType(messageTypeDescription);
+                                                    ((IDictionary<string, object>)message).Remove("MessageTypeFullName");
+
+                                                    //i don't need it yet
+                                                    ((IDictionary<string, object>)message).Remove("CorrelationId");
+                                                    ((IDictionary<string, object>)message).Remove("CorrelationTimeStamp");
+
+                                                    //deserialize
+                                                    var cleanedMessage = JsonConvert.SerializeObject(message);
+                                                    var deserializeObject = JsonConvert.DeserializeObject(cleanedMessage, messageType, new JsonSerializerSettings { MissingMemberHandling = MissingMemberHandling.Error, NullValueHandling = NullValueHandling.Include });
+
+                                                    //add message stream context if possible
+                                                    if (deserializeObject.GetType().BaseType == typeof(DocumentMessage))
+                                                        ((DocumentMessage)deserializeObject).MessageStreamContext = this;
+
+                                                    return deserializeObject;
+                                                }
+                                                catch (Exception error)
+                                                {
+                                                    _messageStream.OnError(error);
+                                                }
+                                                return message;
+                                            })
+                .Subscribe(observer);
         }
     }
 }
