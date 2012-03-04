621d9897dcc20324f6e90fd68725a6c61da06e9e
Mike Bild <mike.bild@googlemail.com>
2012-03-04 16:53:46 +0100

    Update MiniMods/HttpMessageStream/HttpMessageProcessorMinimod.cs

--- MiniMods/HttpMessageStream/HttpMessageProcessorMinimod.cs
+++ MiniMods/HttpMessageStream/HttpMessageProcessorMinimod.cs
@@ -5,8 +5,19 @@ using Minimod.MessageProcessor;
 
 namespace Minimod.HttpMessageStream
 {
+    public class ServerErrorMessage : IMessage
+    {
+        public string Message { get; set; }
+        public HttpContext HttpContext { get; set; }
+    }
+
+    public class RouteFoundMessage : IMessage
+    {
+        public string Uri { get; set; }
+    }
+
     /// <summary>
-    /// Minimod.HttpMessageStream, Version 0.0.3
+    /// Minimod.HttpMessageStream, Version 0.0.4
     /// <para>Small embedded web server based on Rx and working like node.js.</para>
     /// </summary>
     /// <remarks>
@@ -15,13 +26,10 @@ namespace Minimod.HttpMessageStream
     /// </remarks>
     public abstract class HttpMessageProcessor : MessageProcessor.MessageProcessor
     {
-        private readonly Subject<object> _routeFoundStream = new Subject<object>();
+        Subject<RouteFoundMessage> _routeFoundStream = new Subject<RouteFoundMessage>();
+        Subject<ServerErrorMessage> _errorStream = new Subject<ServerErrorMessage>();
         private readonly IDisposable _404Subscription;
-
-        public new void OnReceive<T>(Func<IObservable<T>, IObservable<T>> action)
-        {
-            OnReceive("/", (Func<IObservable<HttpGet>, IObservable<HttpGet>>) action);
-        }
+        IDisposable _500Subscription;
 
         public void OnReceive<T>(string uri, Func<IObservable<T>, IObservable<T>> action)
             where T : class, IMessage
@@ -30,35 +38,45 @@ namespace Minimod.HttpMessageStream
                 .OfType<HttpContext>()
                 .Where(httpContext => httpContext.TryHandleRoute(uri))
                 .Select(httpContext =>
-                            {
-                                switch (httpContext.Request.HttpMethod)
-                                {
-                                    case "GET":
-                                        return new HttpGet(httpContext.ListenerRequest, httpContext.ListenerResponse) as T;
-                                    case "POST":
-                                        return new HttpPost(httpContext.ListenerRequest, httpContext.ListenerResponse) as T;
-                                    case "PUT":
-                                        return new HttpPut(httpContext.ListenerRequest, httpContext.ListenerResponse) as T;
-                                    case "DELETE":
-                                        return new HttpDelete(httpContext.ListenerRequest, httpContext.ListenerResponse) as T;
-                                    case "HEAD":
-                                        return new HttpHead(httpContext.ListenerRequest, httpContext.ListenerResponse) as T;
-                                    case "OPTIONS":
-                                        return new HttpOptions(httpContext.ListenerRequest, httpContext.ListenerResponse) as T;
-                                    case "TRACE":
-                                        return new HttpTrace(httpContext.ListenerRequest, httpContext.ListenerResponse) as T;
-                                    default:
-                                        return new HttpRaw(httpContext.ListenerRequest, httpContext.ListenerResponse) as T;
-                                }
-                            })
+                    {
+                        switch (httpContext.Request.HttpMethod)
+                        {
+                            case "GET":
+                                return new HttpGet(httpContext.ListenerRequest, httpContext.ListenerResponse) as T;
+                            case "POST":
+                                return new HttpPost(httpContext.ListenerRequest, httpContext.ListenerResponse) as T;
+                            case "PUT":
+                                return new HttpPut(httpContext.ListenerRequest, httpContext.ListenerResponse) as T;
+                            case "DELETE":
+                                return new HttpDelete(httpContext.ListenerRequest, httpContext.ListenerResponse) as T;
+                            case "HEAD":
+                                return new HttpHead(httpContext.ListenerRequest, httpContext.ListenerResponse) as T;
+                            case "OPTIONS":
+                                return new HttpOptions(httpContext.ListenerRequest, httpContext.ListenerResponse) as T;
+                            case "TRACE":
+                                return new HttpTrace(httpContext.ListenerRequest, httpContext.ListenerResponse) as T;
+                            default:
+                                return new HttpRaw(httpContext.ListenerRequest, httpContext.ListenerResponse) as T;
+                        }
+                    })
                 .OfType<HttpContext>()
                 .Do(httpContext => httpContext.MapRequestUriArgumentsFromUri(uri))
-                .Do(httpContext => _routeFoundStream.OnNext(httpContext))
+                .Do(httpContext => _routeFoundStream.OnNext(new RouteFoundMessage() { Uri = uri }))
                 .OfType<T>();
 
             action(contextPipe)
-                .OfType<HttpContext>()                
-                .Do(httpContext => httpContext.ExecuteResult().Send(httpContext.ListenerResponse))
+                .OfType<HttpContext>()
+                .Do(httpContext =>
+                    {
+                        try
+                        {
+                            httpContext.ExecuteResult().Send(httpContext.ListenerResponse);
+                        }
+                        catch (Exception error)
+                        {
+                            _errorStream.OnNext(new ServerErrorMessage { Message = error.Message, HttpContext = httpContext });
+                        }
+                    })
                 .Retry()
                 .Subscribe();
         }
@@ -66,25 +84,33 @@ namespace Minimod.HttpMessageStream
         protected HttpMessageProcessor(IObservable<object> messages)
             : base(messages)
         {
-            _404Subscription = Handle404NotFound()
-                .Subscribe();
+            _500Subscription = Handle500ServerError().Subscribe();
+            _404Subscription = Handle404NotFound().Subscribe();
         }
 
         public override void Dispose()
         {
             base.Dispose();
+            _500Subscription.Dispose();
             _404Subscription.Dispose();
         }
 
         private IObservable<object> Handle404NotFound()
         {
             return Stream
-              .OfType<HttpContext>()
-              .TakeUntil(_routeFoundStream)
-              .Repeat()
-              .SkipUntil(Stream)
-              .Do(httpContext => httpContext.Result(() => new EmptyHtmlTextResult(404)))
-              .Do(httpContext => httpContext.ExecuteResult().Send(httpContext.ListenerResponse));
+                .OfType<HttpContext>()
+                .TakeUntil(_routeFoundStream)
+                .Repeat()
+                .SkipUntil(Stream)
+                .Do(httpContext => httpContext.Result(() => new EmptyHtmlTextResult(404)))
+                .Do(httpContext => httpContext.ExecuteResult().Send(httpContext.ListenerResponse));
+        }
+
+        private IObservable<object> Handle500ServerError()
+        {
+            return _errorStream
+                .Do(httpContext => httpContext.HttpContext.Result(() => new StringHtmlTextResult(httpContext.Message, 500)))
+                .Do(httpContext => httpContext.HttpContext.ExecuteResult().Send(httpContext.HttpContext.ListenerResponse));
         }
     }
 }
