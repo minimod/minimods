3c6344882787b07cabc9f06b2b58cc84d22f6ce7
Noam Lewis <lenoam+BDB@gmail.com>
2013-04-12 12:51:44 -0400

    Fixed problems preventing compilation out-of-the-box after cloning: - NuGet.exe: add execution permission - MiniMods/ThreadVariable/RealWorldExample.cs - fix a compilation error due to mismatch between accessability levels.

--- MiniMods/ThreadVariable/RealWorldExample.cs
+++ MiniMods/ThreadVariable/RealWorldExample.cs
@@ -90,7 +90,7 @@ namespace Minimod.ThreadVariable
         }
     }
 
-    public static class SomeThreadVars
+    internal static class SomeThreadVars
     {
         public static readonly ThreadVariable<bool> IsSuperUser = new ThreadVariable<bool>();
     }
